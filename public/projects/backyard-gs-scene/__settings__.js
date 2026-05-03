d8a51f28ff3369352a1843ba8113de5465fa5359
will4799 <wherewini4799@gmail.com>
2026-05-03 21:04:12 +1200

    fix:gaussian still didn't work on mobile. Trying another fix by decreasing gpu usage

--- public/projects/backyard-gs-scene/__settings__.js
+++ public/projects/backyard-gs-scene/__settings__.js
@@ -2,11 +2,11 @@ window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
 window.SCENE_PATH = "2485965.json";
 window.CONTEXT_OPTIONS = {
-    'antialias': true,
+    'antialias': false,
     'alpha': false,
     'preserveDrawingBuffer': false,
     'deviceTypes': [`webgl2`, `webgl1`],
-    'powerPreference': "high-performance"
+    'powerPreference': "default"
 };
 window.SCRIPTS = [ 287547428, 287547736 ];
 window.CONFIG_FILENAME = "config.json";
